8f88ec2277132cc6669b514d701ad1956d6855a2
Marco Mariani <marco@crowdsec.net>
2022-01-31 14:20:37 +0100

    shellcheck

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -7,28 +7,28 @@ if [ "$CONFIG_FILE" != "" ]; then
 fi
 
 # regenerate local agent credentials (ignore if agent is disabled)
-if [ "$DISABLE_AGENT" == "" ] ; then
+if [ "$DISABLE_AGENT" = "" ] ; then
     echo "Regenerate local agent credentials"
     cscli -c "$CS_CONFIG_FILE" machines delete localhost
     cscli -c "$CS_CONFIG_FILE" machines add localhost --auto
     if [ "$AGENT_USERNAME" != "" ] && [ "$AGENT_PASSWORD" != "" ] && [ "$LOCAL_API_URL" != "" ] ; then
         echo "set up lapi credentials for agent"
         CONFIG_PATH=$(yq eval '.api.client.credentials_path' "$CS_CONFIG_FILE" )
-        echo "url: $LOCAL_API_URL" > $CONFIG_PATH
-        echo "login: $AGENT_USERNAME" >> $CONFIG_PATH
-        echo "password: $AGENT_PASSWORD" >> $CONFIG_PATH
+        echo "url: $LOCAL_API_URL" > "$CONFIG_PATH"
+        echo "login: $AGENT_USERNAME" >> "$CONFIG_PATH"
+        echo "password: $AGENT_PASSWORD" >> "$CONFIG_PATH"
     fi
 fi
 
 # Check if lapi needs to automatically register an agent
 echo Check if lapi need to register automatically an agent
-if [ "$DISABLE_LOCAL_API" == "" ] && [ "$AGENT_USERNAME" != "" ] && [ "$AGENT_PASSWORD" != "" ] ; then
-    cscli -c "$CS_CONFIG_FILE" machines add $AGENT_USERNAME --password $AGENT_PASSWORD
+if [ "$DISABLE_LOCAL_API" = "" ] && [ "$AGENT_USERNAME" != "" ] && [ "$AGENT_PASSWORD" != "" ] ; then
+    cscli -c "$CS_CONFIG_FILE" machines add "$AGENT_USERNAME" --password "$AGENT_PASSWORD"
     echo "Agent registered to lapi"
 fi
 
 # registration to online API for signal push
-if [ "$DISABLE_ONLINE_API" == "" ] && [ "$CONFIG_FILE" == "" ] ; then
+if [ "$DISABLE_ONLINE_API" = "" ] && [ "$CONFIG_FILE" = "" ] ; then
     CONFIG_EXIST=$(yq eval '.api.server.online_client | has("credentials_path")' "$CS_CONFIG_FILE")
     if [ "$CONFIG_EXIST" != "true" ]; then
         yq eval '.api.server.online_client = {"credentials_path": "/etc/crowdsec/online_api_credentials.yaml"}' "$CS_CONFIG_FILE" > /etc/crowdsec/config2.yaml
@@ -42,8 +42,8 @@ fi
 if [ "$GID" != "" ]; then
     IS_SQLITE=$(yq eval '.db_config.type == "sqlite"' "$CS_CONFIG_FILE")
     DB_PATH=$(yq eval '.db_config.db_path' "$CS_CONFIG_FILE")
-    if [ "$IS_SQLITE" == "true" ]; then
-        chown :$GID $DB_PATH
+    if [ "$IS_SQLITE" = "true" ]; then
+        chown ":$GID" "$DB_PATH"
         echo "sqlite database permissions updated"
     fi
 fi
@@ -54,15 +54,19 @@ cscli -c "$CS_CONFIG_FILE" collections upgrade crowdsecurity/linux || true
 cscli -c "$CS_CONFIG_FILE" parsers upgrade crowdsecurity/whitelists || true
 cscli -c "$CS_CONFIG_FILE" parsers install crowdsecurity/docker-logs || true
 if [ "$COLLECTIONS" != "" ]; then
+    # shellcheck disable=SC2086
     cscli -c "$CS_CONFIG_FILE" collections install $COLLECTIONS
 fi
 if [ "$PARSERS" != "" ]; then
+    # shellcheck disable=SC2086
     cscli -c "$CS_CONFIG_FILE" parsers install $PARSERS
 fi
 if [ "$SCENARIOS" != "" ]; then
+    # shellcheck disable=SC2086
     cscli -c "$CS_CONFIG_FILE" scenarios install $SCENARIOS
 fi
 if [ "$POSTOVERFLOWS" != "" ]; then
+    # shellcheck disable=SC2086
     cscli -c "$CS_CONFIG_FILE" postoverflows install $POSTOVERFLOWS
 fi
 
@@ -77,23 +81,24 @@ fi
 if [ "$TYPE" != "" ]; then
     ARGS="$ARGS -type $TYPE"
 fi
-if [ "$TEST_MODE" == "true" ] || [ "$TEST_MODE" == "TRUE" ]; then
+if [ "$TEST_MODE" = "true" ] || [ "$TEST_MODE" = "TRUE" ]; then
     ARGS="$ARGS -t"
 fi
-if [ "$DISABLE_AGENT" == "true" ] || [ "$DISABLE_AGENT" == "TRUE" ]; then
+if [ "$DISABLE_AGENT" = "true" ] || [ "$DISABLE_AGENT" = "TRUE" ]; then
     ARGS="$ARGS -no-cs"
 fi
-if [ "$DISABLE_LOCAL_API" == "true" ] || [ "$DISABLE_LOCAL_API" == "TRUE" ]; then
+if [ "$DISABLE_LOCAL_API" = "true" ] || [ "$DISABLE_LOCAL_API" = "TRUE" ]; then
     ARGS="$ARGS -no-api"
 fi
-if [ "$LEVEL_TRACE" == "true" ] || [ "$LEVEL_TRACE" == "TRUE" ]; then
+if [ "$LEVEL_TRACE" = "true" ] || [ "$LEVEL_TRACE" = "TRUE" ]; then
     ARGS="$ARGS -trace"
 fi
-if [ "$LEVEL_DEBUG" == "true" ] || [ "$LEVEL_DEBUG" == "TRUE"  ]; then
+if [ "$LEVEL_DEBUG" = "true" ] || [ "$LEVEL_DEBUG" = "TRUE"  ]; then
     ARGS="$ARGS -debug"
 fi
-if [ "$LEVEL_INFO" == "true" ] || [ "$LEVEL_INFO" == "TRUE" ]; then
+if [ "$LEVEL_INFO" = "true" ] || [ "$LEVEL_INFO" = "TRUE" ]; then
     ARGS="$ARGS -info"
 fi
 
+# shellcheck disable=SC2086
 exec crowdsec $ARGS
